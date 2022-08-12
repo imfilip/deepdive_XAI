316b614e8fab1980400ad7784bcb7bfedadff8cf
imfilip <filip.marcin17@gmail.com>
2022-08-12 16:09:08 +0200

    Basicsf

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+
 if __name__ == "__main__":
     print(True)
 else:
     print(False)
+
+import dalex as dx
+titanic = dx.datasets.load_titanic()
+X = titanic.drop(columns='survived')
+y = titanic.survived
+
+print(X)
+
+preprocess = make_column_transformer(
+    (StandardScaler(), ['age', 'fare', 'parch', 'sibsp']),
+    (OneHotEncoder(), ['gender', 'class', 'embarked']))
+
+print(preprocess)
